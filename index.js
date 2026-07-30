e90e9d46bf5b4fdf8bdd92adec87f7c5efbb9d11
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-07-30 16:51:30 -0500

    Setting up contact and gmail business email

--- index.js
+++ index.js
@@ -6,10 +6,16 @@ const contactRoutes = require('./routes/contactRoutes');
 
 let app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://consoulstudios.com',
+    'https://www.consoulstudios.com',
+  ],
+}));
 app.use(express.json());
 
-app.use('/api/contact', contactRoutes);
+app.use('/pages/contact', contactRoutes);
 
 let PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
