1c17ac757f6719e01b4988a0808e747f1170da7f
MHenderson1990 <morganhenderson_90@yahoo.com>
2026-07-30 16:58:19 -0500

    Forcing port 587

--- config/mailer.js
+++ config/mailer.js
@@ -1,11 +1,14 @@
 const nodemailer = require('nodemailer');
 
 let transporter = nodemailer.createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
+  port: 587,
+  secure: false,            // STARTTLS, not SSL
   auth: {
     user: process.env.GMAIL_USER,
     pass: process.env.GMAIL_APP_PASSWORD,
   },
+  connectionTimeout: 10000, // fail fast instead of hanging
 });
 
 module.exports = transporter;
